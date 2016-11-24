9ad7d9907c86ae6e6ac98d4e80d2eef76d9a6705
Roman Koshelev <romakosh@gmail.com>
2016-11-24 03:32:29 +0300

    reset pos randomly

---
+++ train_002.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+from core.ddpg_mind import DdpgMind
+from core.tensorflow_platform import TensorflowPlatform
+from core.tentacle_agent import TentacleAgent
+from core.tentacle_world import TentacleWorld
+from core.train_procedure import TrainProc
+from core.experiment import Experiment
+
+
+def train_mujoco_tentacle_world():
+    train = TrainProc(TensorflowPlatform, TentacleWorld, TentacleAgent, DdpgMind, episodes=30000, steps=150)
+    exp = Experiment("002", train)
+    exp()
+
+if __name__ == '__main__':
+    train_mujoco_tentacle_world()
