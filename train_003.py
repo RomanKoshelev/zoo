5b388a37ec3c9a5c6b3dce01b008c9da5ec65685
Roman Koshelev <romakosh@gmail.com>
2016-11-25 03:05:32 +0300

    train #003

---
+++ train_003.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+from core.context import Context
+from core.ddpg_mind import DdpgMind
+from core.experiment import Experiment
+from core.tensorflow_platform import TensorflowPlatform
+from core.tentacle_agent import TentacleAgent
+from core.tentacle_world import TentacleWorld
+from core.train_procedure import TrainProc
+from utils.noise_tools import staircase_5
+
+
+def train_mujoco_tentacle_world():
+    Context.config = {
+        'episodes': 30000,
+        'steps': 75,
+        'train.buffer_size': 1e6,
+        'train.noise_sigma': .1,
+        'train.noise_theta': .01,
+        'train.noise_rate_method': staircase_5,
+    }
+
+    train_proc = TrainProc(TensorflowPlatform, TentacleWorld, TentacleAgent, DdpgMind)
+    experiment = Experiment("003", train_proc)
+    experiment()
+
+if __name__ == '__main__':
+    train_mujoco_tentacle_world()
