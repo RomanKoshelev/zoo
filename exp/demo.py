8a39cff74d4a596168cf0ff0f846e5204c0b5da8
Roman Koshelev <romakosh@gmail.com>
2016-12-04 20:12:49 +0300

    demo 001

--- exp/demo.py
+++ exp/demo.py
@@ -14,24 +14,31 @@ from env.tentacle_target import random_target
 
 def train_mujoco_tentacle_world():
     Context.config = {
-        'episodes': 10000,
-        'steps': 75,
+        'exp.episodes': 10000,
+        'exp.steps': 75,
+        'exp.save_every_episodes': 200,
+        'exp.base_path': "../out/experiments",
+
+        'env.model_world_path': "../env/assets/tentacle_world.xml",
+        'env.model_agent_path': "../env/assets/tentacle_agent.xml",
+        'env.reward_method': default_reward,
+        'env.target_mouse_control': False,
+        'env.target_range_xz': [1.1, 0.9],
+        'env.target_location_method': random_target,
+
+        'mind.evaluate_every_episodes': 10,
+
+        'report.write_every_episodes': 5,
+        'report.summary_every_episodes': 20,
+        'report.diagram_mean_frame': 50,
+        'report.refrech_html_every_secs': 30,
 
         'view.width': 1200,
         'view.height': 800,
-
-        'exp.base_path': "../out/experiments",
-
-        'env.world_path': "../env/assets/tentacle_world.xml",
-        'env.agent_path': "../env/assets/tentacle_agent.xml",
-        'env.reward_method': default_reward,
-        'env.target_location_method': random_target,
-        'env.target_range_xz': [1.1, 1.0],
-        'env.target_mouse_control': True,
     }
 
     demo_proc = DemoProc(TensorflowPlatform, TentacleWorld, MujocoAgent, DdpgMind, Reporter)
-    experiment = Experiment("demo", demo_proc, init_from="007")
+    experiment = Experiment("demo", demo_proc, init_from="001")
     experiment.start()
 
 if __name__ == '__main__':
